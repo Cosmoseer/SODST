b9f092fabac2520d9ca5f9f0275134116e558a26
Andrew Phimister <andyph1@hotmail.com>
2026-04-22 22:26:26 +0100

    Work in progress

--- src/orbit_visualiser/main.py
+++ src/orbit_visualiser/main.py
@@ -1,21 +1,22 @@
 import sys
-from tkinter import Tk
+from PyQt6.QtWidgets import QMainWindow, QApplication, QHBoxLayout, QFrame, QWidget, QPushButton
 from orbit_visualiser.core import Orbit, Satellite, CentralBody
 from orbit_visualiser.ui import OrbitFigure, OrbitConfigBuilder, OrbitConfigController
 from orbit_visualiser.ui.common.presets import initial_config
 
-class OrbitVisualiser():
-
+class MainWindow(QMainWindow):
     FIGURE_GEOMETRY = ("left", "nw")
     CONFIG_GEOMETRY = ("right", "ne")
 
-    def __init__(self, root: Tk):
-        root.title("2D Orbit Visualiser")
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("2D Orbit Visualiser")
 
         if sys.platform.startswith("win"):
-            root.state("zoomed")
+            self.showMaximized()
         else:
-            root.state("normal")
+            self.showNormal()
 
         orbit: Orbit = Orbit.from_orbital_elements(
             initial_config.eccentricity,
@@ -30,7 +31,18 @@ class OrbitVisualiser():
         )
         satellite: Satellite = Satellite(orbit.position, orbit.velocity, central_body)
 
-        orbit_figure: OrbitFigure = OrbitFigure(
+        main_layout: QHBoxLayout = QHBoxLayout()
+
+        vars_frame = QFrame(parent = self)
+        main_layout.addWidget(QPushButton())
+        main_layout.addWidget(QPushButton())
+        main_layout.addWidget(QPushButton())
+
+        widget = QWidget()
+        widget.setLayout(main_layout)
+        self.setCentralWidget(widget)
+
+        """ orbit_figure: OrbitFigure = OrbitFigure(
             root, OrbitVisualiser.FIGURE_GEOMETRY, satellite
         )
         orbit_figure.build()
@@ -46,14 +58,15 @@ class OrbitVisualiser():
             orbit_controller.validate_manual_input,
             orbit_controller.slider_changed,
             orbit_controller.format_display_value
-        )
+        ) """
+
+
 
 # TODO: Write tests as I go.
 # TODO: Add variable presets (Earth - ISS, Earth - Geostationary, Mars - Phobos etc).
 # TODO: Write proper docstrings
 if __name__ == "__main__":
-    root = Tk()
-
-    app: OrbitVisualiser = OrbitVisualiser(root)
-
-    root.mainloop()
+    app: QApplication = QApplication([])
+    main = MainWindow()
+    main.show()
+    app.exec()
