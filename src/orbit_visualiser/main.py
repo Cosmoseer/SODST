1758444bca81658b902492d7b91a707821a1df21
Andrew Phimister <157378252+Phim226@users.noreply.github.com>
2026-05-05 17:00:32 +0100

    Wrote TODO

--- src/orbit_visualiser/main.py
+++ src/orbit_visualiser/main.py
@@ -4,6 +4,7 @@ from orbit_visualiser.core import Orbit, Satellite, CentralBody
 from orbit_visualiser.ui import UIController, UIBuilder, OrbitDataAccess, GeometryManager, initial_config
 
 # TODO: Replace gif in README
+# TODO: Update propagation code and README description
 class OrbitVisualiser():
 
     def __init__(self, root: Window, geo_manager: GeometryManager):
