1ebf4a5d5b2adf1005673d86aa356070c23cc71f
Andrew Phimister <157378252+Phim226@users.noreply.github.com>
2026-05-05 16:49:19 +0100

    Geometry manager included in ui init

--- src/orbit_visualiser/ui/__init__.py
+++ src/orbit_visualiser/ui/__init__.py
@@ -1,4 +1,4 @@
 from .ui_controller import UIController
-from .ui_builder import UIBuilder
+from .ui_builder import UIBuilder, GeometryManager
 from orbit_visualiser.ui.common.presets import initial_config
 from orbit_visualiser.ui.data_access import OrbitDataAccess
