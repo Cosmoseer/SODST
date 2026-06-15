04e6eb7cd47c2937c061a2b3149e05608263577b
Andrew Phimister <andyph1@hotmail.com>
2026-06-15 16:55:19 +0100

    Docstring written for Gibbs determination algorithm

--- src/orbit_visualiser/core/astrodynamics/determination/gibbs.py
+++ src/orbit_visualiser/core/astrodynamics/determination/gibbs.py
@@ -1,16 +1,37 @@
 import numpy as np
 from numpy.typing import NDArray
 from orbit_visualiser.core.orbit import Orbit
+from orbit_visualiser.core.astrodynamics.determination.determination_error import DeterminationError
 
 def gibbs_orbit_determination(
         r_1: NDArray[np.float64], r_2: NDArray[np.float64], r_3: NDArray[np.float64], mu: float
 ) -> Orbit:
-    v_1 = _gibbs_velocity(r_1, r_2, r_3, mu)
-    return Orbit(r_1, v_1, mu)
+    """
+    Uses the Gibbs orbit determination algorithm to calculate the velocity vector at r_1, and
+    returns an Orbit object.
 
-def _gibbs_velocity(
-        r_1: NDArray[np.float64], r_2: NDArray[np.float64], r_3: NDArray[np.float64], mu: float
-) -> NDArray[np.float64]:
+    Parameters
+    ----------
+    r_1 : NDArray[np.float64]
+        First position vector (km)
+    r_2 : NDArray[np.float64]
+        Second position vector (km)
+    r_3 : NDArray[np.float64]
+        Third position vector (km)
+    mu : float
+        Gravitational parameter of the central body (km^3/s^2)
+
+    Returns
+    -------
+    Orbit
+       Orbit object, taking the first position vector and its calculated velocity vector as its arguments.
+
+    Raises
+    ------
+    DeterminationError
+        The Gibbs algorithm relies on the position vectors being pairwise non-collinear. If that
+        condition isn't satisfied then the custom error DeterminationError is thrown.
+    """
     r1_norm = np.linalg.norm(r_1)
     r2_norm = np.linalg.norm(r_2)
     r3_norm = np.linalg.norm(r_3)
@@ -26,5 +47,7 @@ def _gibbs_velocity(
     D = c_12 + c_23 + c_31
     D_norm = np.linalg.norm(D)
     S = (r2_norm - r3_norm)*r_1 + (r3_norm - r1_norm)*r_2 + (r1_norm - r2_norm)*r_3
-    return np.sqrt(mu/(N_norm*D_norm))*(np.cross(D, r_1)/r1_norm + S)
+    v_1 = np.sqrt(mu/(N_norm*D_norm))*(np.cross(D, r_1)/r1_norm + S)
+
+    return Orbit(r_1, v_1, mu)
 
